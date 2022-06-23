5f00a55bbc2819163cbc3933f3a6cedaa02175c3
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2022-06-23 10:40:26 +0530

    dynamicc ss.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { useState } from "react";
+
 function App() {
-  let myref = { color: "tomato", background: "yellow" };
+  let [counter, setCounter] = useState(0);
+  let myref = {
+    color: "tomato",
+    background: counter == 0 ? "yellow" : "skyblue",
+  };
+
+  const increment = () => {
+    setCounter(counter + 1);
+  };
 
   return (
     <div>
+      <input type="button" value="Click Me!" onClick={increment} />
+
       <h1 style={myref}>Hello World</h1>
+
+      <h1
+        style={{ color: "red", background: counter == 0 ? "yellow" : "blue" }}
+      >
+        Dynamic CSS
+      </h1>
 
       <h1 style={{ color: "red", background: "yellow" }}>
         Counter application
