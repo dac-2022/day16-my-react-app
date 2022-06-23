a9e2849cf4b0a61d595e0a7a69c76741968f72ed
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2022-06-23 10:53:55 +0530

    bootstrap integration.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
     <div>
-      <h1 className="myh1">Hello External Style</h1>
-      <h1 style={{ color: "red" }}>Hello World</h1>
+      <h1>Hello World</h1>
     </div>
   );
 }
