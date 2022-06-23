81cd3c348476c10ae8742eefd112787ebcadf9b3
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2022-06-23 10:48:36 +0530

    external styling.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
-import { useState } from "react";
+import "./App.css";
 
 function App() {
-  let [counter, setCounter] = useState(0);
-  let myref = {
-    color: "tomato",
-    background: counter == 0 ? "yellow" : "skyblue",
-  };
-
-  const increment = () => {
-    setCounter(counter + 1);
-  };
-
   return (
     <div>
-      <input type="button" value="Click Me!" onClick={increment} />
-
-      <h1 style={myref}>Hello World</h1>
-
-      <h1
-        style={{ color: "red", background: counter == 0 ? "yellow" : "blue" }}
-      >
-        Dynamic CSS
-      </h1>
-
-      <h1 style={{ color: "red", background: "yellow" }}>
-        Counter application
-      </h1>
+      <h1 className="myh1">Hello External Style</h1>
+      <h1 style={{ color: "red" }}>Hello World</h1>
     </div>
   );
 }
