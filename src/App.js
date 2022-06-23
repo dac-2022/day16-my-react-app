d9d17005295316fdce6b4c35f07f166148596825
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2022-06-23 11:04:39 +0530

    basics bootrap.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
-import "bootstrap/dist/css/bootstrap.css";
-
 function App() {
   return (
-    <div>
-      <h1 className="bg-primary text-light p-3">Hello World</h1>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col bg-primary text-light p-3">
+          <h1>Hello World</h1>
+        </div>
+      </div>
+
+      <div className="row mb-1">
+        <div className="col p-0">
+          <div className="alert alert-primary mb-0">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut alias
+            delectus labore maxime, natus sequi cupiditate adipisci officiis,
+            ullam necessitatibus harum, laudantium cum. Aspernatur odit officia
+            rerum tenetur fugiat enim?
+          </div>
+        </div>
+      </div>
+
+      <div className="row mb-1">
+        <div className="col p-0">
+          <div className="alert alert-primary mb-0">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut alias
+            delectus labore maxime, natus sequi cupiditate adipisci officiis,
+            ullam necessitatibus harum, laudantium cum. Aspernatur odit officia
+            rerum tenetur fugiat enim?
+          </div>
+        </div>
+      </div>
+
+      <div className="row mb-1">
+        <div className="col p-0">
+          <div className="alert alert-primary ">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut alias
+            delectus labore maxime, natus sequi cupiditate adipisci officiis,
+            ullam necessitatibus harum, laudantium cum. Aspernatur odit officia
+            rerum tenetur fugiat enim?
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
