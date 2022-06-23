3ea62afa9f25fd6757fc488addf996cb900b6ba9
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2022-06-23 11:22:36 +0530

    working with list.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
+import { useState } from "react";
+
 function App() {
-  let list = Array.from({ length: 100 });
+  let [list, setList] = useState([]);
+
+  const addNewRow = () => {
+    const newList = [...list, 1];
+    setList(newList);
+  };
+
   return (
     <div className="container-fluid">
-      <div className="row">
-        <div className="col bg-primary text-light p-3">
+      <div className="row bg-primary align-items-center">
+        <div className="col text-light p-3">
           <h1>Hello World</h1>
+        </div>
+        <div className="col ">
+          <input type="button" value="Add New Row" onClick={addNewRow} />
         </div>
       </div>
 
       {list.map((item, index) => (
-        <div key={index} className="row mb-1">
-          <div className="col p-0">
-            <div className="alert alert-primary mb-0">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut alias
-              delectus labore maxime, natus sequi cupiditate adipisci officiis,
-              ullam necessitatibus harum, laudantium cum. Aspernatur odit
-              officia rerum tenetur fugiat enim?
-            </div>
+        <div key={index} className="row">
+          <div className="col alert alert-primary mb-1">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae eius
+            architecto amet tempore facilis aliquid odio alias incidunt error,
+            earum, delectus ipsa itaque dolore, suscipit expedita cupiditate
+            voluptatum? Laudantium, sint.
           </div>
         </div>
       ))}
