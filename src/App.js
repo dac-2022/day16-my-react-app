a921674a84419c7a8900d7e3490acf9546c633b8
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2022-06-23 14:53:24 +0530

    hour.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,31 @@
-import { useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  let textRef = useRef();
-  let [userList, setUserList] = useState([]);
+  // You have to use Date Object.
+  let [hour, setHour] = useState(2);
+  let [second, setSecond] = useState(0);
+  let [minute, setMinute] = useState(52);
 
-  const clickMe = () => {
-    const newValue = textRef.current.value;
-    const newList = [newValue, ...userList];
-    setUserList(newList);
+  // Constructor ::
+  useEffect(() => {
+    let clock1 = setInterval(() => {
+      if (second == 60) {
+        setMinute(minute + 1);
+        setSecond(0);
+      } else {
+        setSecond((prevValue) => prevValue + 1);
+      }
+    }, 1000);
 
-    textRef.current.value = "";
-  };
+    return () => clearInterval(clock1);
+  }, [second, minute]);
 
   return (
     <div>
-      <h1 className="bg-primary p-3 text-light">Hello World</h1>
-
-      <input
-        className="form-control form-control-lg"
-        ref={textRef}
-        type="text"
-      />
-      <input
-        className="btn btn-lg btn-primary w-100 my-1"
-        type="button"
-        value="Cick Me"
-        onClick={clickMe}
-      />
-
-      {userList.map((item, index) => (
-        <h1 className="alert alert-primary" key={index}>
-          {item}
-        </h1>
-      ))}
+      <h1>Clock App</h1>
+      <h1>
+        {hour}: {minute} : {second}
+      </h1>
     </div>
   );
 }
