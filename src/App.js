2f439e4e05c701ceb2162d085ed306cd8ee9eb85
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2022-06-23 10:54:49 +0530

    simple botstrap integationasdf

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.css";
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1 className="bg-primary text-light p-3">Hello World</h1>
     </div>
   );
 }
